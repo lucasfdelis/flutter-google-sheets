08317b29807e2e30b6832ea91c61f5571cbc68ed
Lucas Alves <lucasalvesf78@gmail.com>
2021-12-13 11:17:49 -0300

    7:51

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application/api/sheets/sales_sheet_api.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await UserSheetsApi.init();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Google Sheets API';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container());
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,6 @@
+class UserFields {
+  static final String Data = 'Data';
+  static final String Nome = 'Nome';
+  static final String Produto = 'Produto';
+  static final String Pagamento = 'Pagamento';
+}

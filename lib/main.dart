a101b6af3840436078230186a870cf30155a5483
Lucas Alves <lucasalvesf78@gmail.com>
2022-01-24 10:54:25 -0300

    Correçao de erros

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_application/api/sheets/sales_sheet_api.dart';
 import 'package:flutter_application/store/tema.store.dart';
 import 'package:flutter_application/widget/button_widget.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_application/model/user.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_application/widget/button_widget.dart';
 
 final store = TemaStore();
 
@@ -54,21 +51,19 @@ class MyApp extends StatelessWidget {
 class CreateSheetsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Observer(
-      builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: Text('Vendas'),
-          centerTitle: true,
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(32),
-          // color: store.clicado ? Colors.white : Colors.black12,
-          child: UserFormWidget(
-            onSavedUser: (user) async {
-              await UserSheetsApi.insert([user.toJson()]);
-            },
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vendas'),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(32),
+        // color: store.clicado ? Colors.white : Colors.black12,
+        child: UserFormWidget(
+          onSavedUser: (user) async {
+            await UserSheetsApi.insert([user.toJson()]);
+          },
         ),
       ),
     );
